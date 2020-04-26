27db088d8ac30e88f9ba5f25b33fd78edea22678
Ryan Styron <ryanstyron1220@gmail.com>
2020-04-26 18:33:17 -0400

    Add a EntityDropItemEvent listener to ListenerMoshpitSpawn.java.

--- src/mc/rysty/heliosphereworld/moshpit/ListenerMoshpitSpawn.java
+++ src/mc/rysty/heliosphereworld/moshpit/ListenerMoshpitSpawn.java
@@ -1,13 +1,16 @@
 package mc.rysty.heliosphereworld.moshpit;
 
 import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
 import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityDropItemEvent;
 
 import mc.rysty.heliosphereworld.HelioSphereWorld;
 
@@ -40,5 +43,19 @@ public class ListenerMoshpitSpawn implements Listener {
             if (entityLocation.distanceSquared(world.getSpawnLocation()) <= 361
                     || damagerLocation.distanceSquared(world.getSpawnLocation()) <= 361)
                 event.setCancelled(true);
+    }
+
+    @EventHandler
+    public void onEntityDropItem(EntityDropItemEvent event) {
+        Entity entity = event.getEntity();
+        World world = entity.getWorld();
+
+        if (world.equals(Bukkit.getWorld("Moshpit")))
+            if (entity instanceof Player) {
+                Player player = (Player) entity;
+
+                if (player.getGameMode() != GameMode.CREATIVE)
+                    event.setCancelled(true);
+            }
     }
 }
