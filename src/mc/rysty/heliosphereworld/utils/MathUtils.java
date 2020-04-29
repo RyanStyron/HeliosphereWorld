da3da54fdc0590cacec4254f0c5398a0532bf7e0
Ryan Styron <ryanstyron1220@gmail.com>
2020-04-29 19:32:06 -0400

    Remove unused method in MathUtils.java.

--- src/mc/rysty/heliosphereworld/utils/MathUtils.java
+++ src/mc/rysty/heliosphereworld/utils/MathUtils.java
@@ -11,16 +11,4 @@ public class MathUtils {
 
         return largest;
     }
-
-    // I don't remember what this was for
-    public static String formatBytes(long bytes) {
-        String s = (bytes < 0L) ? "-" : "";
-        long b = (bytes == Long.MIN_VALUE) ? Long.MAX_VALUE : Math.abs(bytes);
-        return (b < 1000L) ? (bytes + " B") : ((b < 999950L) ?
-                String.format("%s%.1f kB", new Object[] { s, Double.valueOf(b / 1000.0D) }) : (((b /= 1000L) < 999950L) ?
-                String.format("%s%.1f MB", new Object[] { s, Double.valueOf(b / 1000.0D) }) : (((b /= 1000L) < 999950L) ?
-                String.format("%s%.1f GB", new Object[] { s, Double.valueOf(b / 1000.0D) }) : (((b /= 1000L) < 999950L) ?
-                String.format("%s%.1f TB", new Object[] { s, Double.valueOf(b / 1000.0D) }) : (((b /= 1000L) < 999950L) ?
-                String.format("%s%.1f PB", new Object[] { s, Double.valueOf(b / 1000.0D) }) : String.format("%s%.1f EB", new Object[] { s, Double.valueOf(b / 1000000.0D) }))))));
-    }
 }
