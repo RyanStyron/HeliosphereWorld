e086008da76815227dd059725155fa6341908095
Ryan Styron <ryanstyron1220@gmail.com>
2020-05-20 16:02:27 -0400

    Update ListenerMoshpitStats.java so that kill streaks are also measured.

--- src/mc/rysty/heliosphereworld/moshpit/ListenerMoshpitStats.java
+++ src/mc/rysty/heliosphereworld/moshpit/ListenerMoshpitStats.java
@@ -12,6 +12,7 @@ import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 import mc.rysty.heliosphereworld.HelioSphereWorld;
+import mc.rysty.heliosphereworld.utils.MessageUtils;
 import mc.rysty.heliosphereworld.utils.MoshpitFileManager;
 
 public class ListenerMoshpitStats implements Listener {
@@ -31,24 +32,37 @@ public class ListenerMoshpitStats implements Listener {
             Player killer = event.getEntity().getKiller();
             GameMode creative = GameMode.CREATIVE;
 
-            if (player.getGameMode() != creative || killer.getGameMode() != creative) {
+            if (player.getGameMode() != creative && killer.getGameMode() != creative) {
                 UUID playerId = player.getUniqueId();
                 UUID killerId = killer.getUniqueId();
                 String playerDisplayname = player.getDisplayName();
+                String killerDisplayname = killer.getDisplayName();
                 double playerKills = moshpitFile.getDouble("users." + playerId + ".kills");
                 double playerDeaths = moshpitFile.getDouble("users." + playerId + ".deaths");
                 double killerKills = moshpitFile.getDouble("users." + killerId + ".kills");
                 double killerDeaths = moshpitFile.getDouble("users." + killerId + ".deaths");
+                double killerStreak = moshpitFile.getDouble("users." + killerId + ".killstreak");
 
                 if (moshpitFile.getString("users." + playerId + ".displayname") != playerDisplayname)
                     moshpitFile.set("users." + playerId + ".displayname", playerDisplayname);
+                if (moshpitFile.getString("users." + killerId + ".displayname") != killerDisplayname)
+                    moshpitFile.set("users." + killerId + ".displayname", killerDisplayname);
                 moshpitFile.set("users." + playerId + ".deaths", playerDeaths + 1.0);
                 moshpitFile.set("users." + playerId + ".kdr",
                         playerDeaths != 0.0 ? playerKills / playerDeaths : playerKills);
+                moshpitFile.set("users." + playerId + ".killstreak", 0.0);
                 moshpitFile.set("users." + killerId + ".kills", killerKills + 1.0);
                 moshpitFile.set("users." + killerId + ".kdr",
                         killerDeaths != 0.0 ? killerKills / killerDeaths : killerKills);
+                moshpitFile.set("users." + killerId + ".killstreak", killerStreak + 1.0);
                 moshpitFileManager.saveData();
+
+                int newKillStreak = (int) killerStreak + 1;
+                if (newKillStreak % 5 == 0)
+                    for (Player onlinePlayer : Bukkit.getOnlinePlayers())
+                        if (onlinePlayer.getWorld().equals(Bukkit.getWorld("Moshpit")))
+                            MessageUtils.message(onlinePlayer, "&3&l(!)&f " + killerDisplayname
+                                    + " &fhas a kill streak of &b" + newKillStreak + "&f!");
             }
         }
     }
